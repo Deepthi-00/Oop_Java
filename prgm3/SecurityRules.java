62eaa37fa4517bc0b2f48bffba0a754c2f6a587e
Deepthi-00 <deepupoojary369@gmail.com>
2025-09-02 08:48:14 +0530

    Add files via upload

---
+++ prgm3/SecurityRules.java
@@ -0,0 +1,13 @@
+package transport;
+
+public final class SecurityRules {
+
+	public SecurityRules() {
+		
+		 } 
+		 public static boolean canFly(String place) { 
+		        return !place.equalsIgnoreCase("ExamCell");
+		// TODO Auto-generated constructor stub
+	}
+
+}
